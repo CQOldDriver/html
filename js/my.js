f43b13ae43aa388f91d577eefa98830f8585a5ab
lyx <609370047@qq.com>
2017-06-11 15:16:42 +0800

    第一次

---
+++ js/my.js
@@ -0,0 +1,168 @@
+//轮播
+/////////////////////////////////轮播图1
+$(function(){
+	var $imgs = $(".lunbo1 li"),
+		len = $imgs.length,
+		currentIndex = 1,
+		nextIndex = 2,
+		timer = null,
+		imgWidth = $imgs.outerWidth(),
+		html = "",
+		isMoving = false;
+
+	
+	var first = $imgs.eq(0).clone(true),
+		last = $imgs.eq(len - 1).clone(true);
+	$(".lunbo1 ul").append(first).prepend(last);
+	len+=2;
+	$(".lunbo1 ul").css({
+		width:len * imgWidth,
+		left: -imgWidth
+	});
+	
+	
+//	$(".lunbo1").mouseenter(function(){
+//		clearInterval(timer)
+//	}).mouseleave(function(){
+//		timer = setInterval(move,3000);
+//	});
+	$(".lunbo1").hover(function(){
+		//mouseenter
+		clearInterval(timer);
+		$(".prev1").css("opacity","1");
+		$(".next1").css("opacity","1");
+	},function(){
+		//mouseleave
+		timer = setInterval(move,2000);
+		$(".prev1").css("opacity","0");
+		$(".next1").css("opacity","0");
+	}).trigger("mouseleave");//指明要触发鼠标移开的事件，移开后执行mouseleave事件的函数，没有trigger()方法的话前面要写var timer = setInterval(move,2000);
+	
+	for(i=0;i<len-2;i++){
+		var j = i+1;
+		html += "<div>"+j+"</div>";
+	}
+	$(html).appendTo(".pages").eq(0).addClass("current");
+	
+	$(".pages").on("mouseover","div",function(){
+		nextIndex = $(this).index() + 1;
+		move();
+	})
+//	$(".pages div").click(function(){
+//				nextIndex = $(this).index() + 1;
+//				move();
+//			});
+	
+	$(".prev1").click(function(){
+		if(!isMoving){
+			nextIndex = currentIndex - 1;
+			move();
+		}
+		
+	});
+	$(".next1").click(function(){
+		if(!isMoving){
+			move();
+		}
+		
+	});
+	
+	function move(){
+		isMoving = true;
+		var _left = -1 * imgWidth * nextIndex;
+		$(".lunbo1 ul").stop().animate({left:_left},function(){
+			isMoving = false;
+			if(nextIndex >= len){
+				$(".lunbo1 ul").css({
+					left: -imgWidth
+				});
+				currentIndex = 1;
+				nextIndex = 2;
+			}else if(currentIndex <= 0){
+				$(".lunbo1 ul").css({
+					left: -imgWidth * (len - 2)
+				});
+				currentIndex = len - 2;
+				nextIndex = len - 1;
+			}
+		});
+		
+		var circleIndex = (nextIndex == len - 1)? 0 : (nextIndex-1);
+		$(".lunbo1 .pages div").eq(circleIndex)
+							   .addClass("current").siblings().removeClass("current");
+		currentIndex = nextIndex;
+		nextIndex++;
+	}
+});
+
+
+/////////////////////////////////轮播图2
+
+//$(function(){
+//	var $imgs = $(".lunbo2 li"),
+//		len = $imgs.length,
+//		currentIndex = 1,
+//		nextIndex = 2,
+//		timer = null,
+//		imgWidth = $imgs.outerWidth(),
+//		html = "",
+//		isMoving = false;
+//	
+//	var first = $imgs.eq(0).clone(true),
+//		last = $imgs.eq(len - 1).clone(true);
+//	$(".lunbo2 ul").append(first).prepend(last);
+//	len+=2;
+//	console.log(imgWidth);
+//	$(".lunbo2 ul").css({
+//		width:len * imgWidth,
+//		left: -imgWidth
+//	});
+//	
+//	$(".lunbo2").hover(function(){
+//		//mouseenter
+//		clearInterval(timer);
+//		$(".prev2").css("opacity","1");
+//		$(".next2").css("opacity","1");
+//	},function(){
+//		//mouseleave
+//		timer = setInterval(move,2000);
+//		$(".prev2").css("opacity","0");
+//		$(".next2").css("opacity","0");
+//	}).trigger("mouseleave");//指明要触发鼠标移开的事件，移开后执行mouseleave事件的函数，没有trigger()方法的话前面要写var timer = setInterval(move,2000);
+//	
+//	
+//	$(".prev2").click(function(){
+//		if(!isMoving){
+//			nextIndex = currentIndex - 1;
+//			move();
+//		}
+//	});
+//	$(".next2").click(function(){
+//		if(!isMoving){
+//			move();
+//		}
+//	});
+//	
+//	function move(){
+//		isMoving = true;
+//		var _left = -1 * imgWidth * nextIndex;
+//		$(".lunbo2 ul").stop().animate({left:_left},function(){
+//			isMoving = false;
+//			if(nextIndex >= len){
+//				$(".lunbo2 ul").css({
+//					left: -imgWidth
+//				});
+//				currentIndex = 1;
+//				nextIndex = 2;
+//			}else if(currentIndex <= 0){
+//				$(".lunbo2 ul").css({
+//					left: -imgWidth * (len - 2)
+//				});
+//				currentIndex = len - 2;
+//				nextIndex = len - 1;
+//			}
+//		});
+//		currentIndex = nextIndex;
+//		nextIndex++;
+//	}
+//});
